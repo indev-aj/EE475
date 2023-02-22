a379e30f285796882bbc1ec6a5f2e7a7028bce27
indev-aj <amrin.wan@gmail.com>
2023-02-22 21:32:25 +0000

    Added code for stage manager[untested]

--- ESP32StageManagerEE.py
+++ ESP32StageManagerEE.py
@@ -3,26 +3,62 @@ from .PositionerManager import PositionerManager
 import time
 import numpy as np
 
-PHYS_FACTOR = 1
-gTIMEOUT = 10
+
 class ESP32StageManagerEE(PositionerManager):
 
     def __init__(self, positionerInfo, name, **lowLevelManagers):
         super().__init__(positionerInfo, name, initialPosition={
             axis: 0 for axis in positionerInfo.axes
         })
+
         self._rs232manager = lowLevelManagers['rs232sManager'][
             positionerInfo.managerProperties['rs232device']
         ]
+
         self.__logger = initLogger(self, instanceName=name)
+        self.board = self._rs232manager._esp32
 
-    
-    def move(self, dist: float, axis: str):
-        pass
+    def move(self, value: float, axis: str):
+        if axis == 'X':
+            self.board.move_rel((value, 0, 0), blocking=False)
+        elif axis == 'Y':
+            self.board.move_rel((0, value, 0), blocking=False)
+        elif axis == 'Z':
+            self.board.move_rel((0, 0, value), blocking=False)
+        else:
+            self.__logger.warning('Wrong axis, has to be "X" "Y" or "Z"')
+            return
+        self._position[axis] = self._position[axis] + value
 
-    
-    def setPosition(self, position: float, axis: str):
-        pass
+    def setPosition(self, value: float, axis: str):
+        self._position[axis] = value
+
+    # test arrow key input
+    from PyQt5 import QtCore
+
+    @shortcut(QtCore.Qt.Key_Up, "Move up")
+    def key_moveXup(self):
+        self.move(value=100, axis="X")
+
+    @shortcut(QtCore.Qt.Key_Down, "Move down")
+    def key_moveXdown(self):
+        self.move(value=-100, axis="X")
+
+    @shortcut(QtCore.Qt.Key_Left, "Move left")
+    def key_moveYleft(self):
+        self.move(value=-100, axis="Y")
+
+    @shortcut(QtCore.Qt.Key_Right, "Move right")
+    def key_moveYright(self):
+        self.move(value=100, axis="Y")
+
+    @shortcut("-", "Move Z up")
+    def key_moveZup(self):
+        self.move(value=100, axis="Z")
+
+    @shortcut("+", "Move Z down")
+    def key_moveZdown(self):
+        self.move(value=-100, axis="Z")
 
 # Copyright (C) 2020, 2021 The imswitch developers
 # This file is part of ImSwitch.
--- esp32driver.py
+++ esp32driver.py
@@ -1,31 +1,34 @@
 import serial
 import glob
 import sys
+import numpy as np
+
 
 from imswitch.imcommon.model import initLogger
 
+
 class ESP32Driver:
-  def __init__(self, address):
-    self.__logger = initLogger(self, tryInheritParent=False)
-    try:
-      self.serial = serial.Serial(address, baudrate=115200, timeout=0.050)
-      self.__logger.debug("ESP32 connected")
-      self.is_connected = True
-    except:
-      self.__logger.warning("No ESP32 connected - Check port?")
-      self.is_connected = False
-    
-    if self.is_connected:
-      self._write("<L1OF,L2OF")
-    
-    # illumination settings
-    self.laser_intensity = 0
-    self.led_state = 0
+    def __init__(self, address):
+        self.__logger = initLogger(self, tryInheritParent=False)
+        try:
+            self.serial = serial.Serial(
+                address, baudrate=115200, timeout=0.050)
+            self.__logger.debug("ESP32 connected")
+            self.is_connected = True
+        except:
+            self.__logger.warning("No ESP32 connected - Check port?")
+            self.is_connected = False
 
-    # self.is_debug = True
+        if self.is_connected:
+            self._write("<L1OF,L2OF")
 
+        # illumination settings
+        self.laser_intensity = 0
+        self.led_state = 0
 
-  def serial_ports(self):
+        # self.is_debug = True
+
+    def serial_ports(self):
         if sys.platform.startswith('win'):
             ports = ['COM%s' % (i + 1) for i in range(256)]
         elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
@@ -46,22 +49,29 @@ class ESP32Driver:
                 pass
         return result
 
-  def _write(self, command):
-    # if self.is_debug: print(command)
-    if self.is_connected:
-      self.serial.write(command.encode())
+    def _write(self, command):
+        # if self.is_debug: print(command)
+        if self.is_connected:
+            self.serial.write(command.encode())
 
-  def set_laser_intensity(self, intensity):
+    def close(self):
+        self.serial.close()
+
+    # --------------
+    # Code for stage
+    # --------------
+
+    def set_laser_intensity(self, intensity):
         self.laser_intensity = intensity
         if self.led_state:
             suffix = "ON"
         else:
             suffix = "OF"
-        
+
         cmd = "<L1" + suffix + ">"
         return self._write(cmd)
 
-  def set_led(self, channel, state=1):
+    def set_led(self, channel, state=1):
         # state is either 1 or 0
         self.led_state = state
         if self.led_state:
@@ -69,12 +79,32 @@ class ESP32Driver:
         else:
             suffix = "OF"
 
-        
         cmd = "<L" + str(channel) + suffix + ">"
         return self._write(cmd)
 
-  def close(self):
-      self.serial.close()
+    # --------------
+    # Code for stage
+    # --------------
+
+    def move_rel(self, position_rel=(0, 0, 0), blocking=True, pingwait=0.25):
+        """Move axises relaitve to current position"""
+        self.move_xyz(position_rel, blocking=blocking, config="rel")
+
+    def move_abs(self, position_rel=(0, 0, 0), blocking=True, pingwait=0.25):
+        """Move axises absolute"""
+        self.move_xyz(position_rel, blocking=blocking, config="abs")
+
+    def move_xyz(self, position=(0, 0, 0), blocking=True, pingwait=0.25, config="abs"):
+        """Move axis with optional blocking"""
+        self.get_positions()
+        if config == "abs":
+            to_go = np.array(position)
+        elif config == "rel":
+            to_go = np.array((position[0]+self.positions[0],
+                              position[1]+self.positions[1],
+                              position[2]+self.positions[2]))
+
+
 
 if __name__ == "__main__":
 
